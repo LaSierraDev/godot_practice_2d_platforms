071cb1f16638f3d7db7177aac468ac926d3dcd19
José de la Sierra <lasierradev@gmail.com>
2026-02-20 13:10:51 +0100

    [FEAT] Coyote time implemented

--- scripts/Autoloads/global.gd
+++ scripts/Autoloads/global.gd
@@ -1,16 +1,21 @@
 extends Node
 
-# --- Player movement --- #
+## --- Player movement --- ##
 const M_RIGHT: String = "right"
 const M_LEFT: String = "left"
 
-# --- Player action --- #
+## --- Player action --- ##
 const A_JUMP: String = "jump"
 
-# --- Player movement animation --- #
+## --- Player movement animation --- ##
 const MA_IDLE: String = "idle"
 const MA_RUN: String = "run"
 
-# --- Player action animation --- #
+## --- Player action animation --- ##
 const AA_JUMP: String = "jump"
 const AA_FALL: String = "fall"
+
+## --- Groups --- ## 
+const G_ENEMY: String = "enemy"
+const G_FLOOR: String = "floor"
+const G_PLAYER: String = "player"
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,25 @@
 extends CharacterBody2D
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite
+@onready var coyote_trigger_area_2d: Area2D = $CoyoteTriggerArea2D
 
 var x_input: float
 var is_facing_right: bool = true
+var is_coyote_time: bool = false
 
 @export var speed: float = 180.0
-@export var jump_impulse: float = 330.0
+@export var jump_impulse: float = 360.0
 @export var gravity: float = 980.0
+@export var coyote_time: float = 0.1
+
+
+func _ready() -> void:
+	coyote_trigger_area_2d.body_exited.connect(_on_body_exited)
+	coyote_trigger_area_2d.body_entered.connect(_on_body_entered)
+
+
+func _process(delta: float) -> void:
+	_update_animation()
 
 
 func _physics_process(delta: float) -> void:
@@ -16,8 +28,6 @@ func _physics_process(delta: float) -> void:
 	_flip()
 	_player_movement()
 	move_and_slide()
-	
-	_update_animation()
 
 
 func _flip() -> void:
@@ -32,7 +42,7 @@ func _player_movement() -> void:
 
 
 func _jump() -> void: 
-	if is_on_floor():
+	if is_on_floor() or is_coyote_time:
 		if Input.is_action_just_pressed(Global.A_JUMP):
 			self.velocity.y = -jump_impulse # Es lo mismo que multiplicarlo por -1
 
@@ -40,6 +50,7 @@ func _jump() -> void:
 func _gravity(delta: float) -> void:
 	if not is_on_floor():
 		self.velocity.y += (gravity * delta)
+
 
 func _update_animation() -> void:
 	if self.velocity.x != 0:
@@ -52,3 +63,21 @@ func _update_animation() -> void:
 	
 	if self.velocity == Vector2(0,0):
 		animated_sprite.play(Global.MA_IDLE)
+
+
+func _deactive_coyote_time() -> void:
+	print("Ya no estoy en el suelo y me estoy cayendo")
+	await get_tree().create_timer(coyote_time).timeout 
+	is_coyote_time = false
+	print("Coyote time desactivado")
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group(Global.G_FLOOR) and self.velocity.y >= 0:
+		is_coyote_time = true
+		_deactive_coyote_time()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group(Global.G_FLOOR):
+		is_coyote_time = false
