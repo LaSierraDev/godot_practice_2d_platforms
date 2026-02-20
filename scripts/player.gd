2534eed8f812544ac17ef5803d822f6c2d949814
José de la Sierra <lasierradev@gmail.com>
2026-02-20 09:51:12 +0100

    [REFACT] Refactor in player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,15 +8,18 @@ var is_facing_right: bool = true
 @export var jump_impulse: float = 50
 
 func _physics_process(delta: float) -> void:
+	print(self.velocity)
 	if Input.is_action_just_pressed(Global.A_JUMP):
 		velocity.y = jump_impulse
 	
+	_flip()
+	_player_movement()
+	move_and_slide()
+
+func _flip() -> void:
 	if (is_facing_right and self.velocity.x < 0) or (not is_facing_right and self.velocity.x > 0):
 		self.scale.x *= -1
 		is_facing_right = not is_facing_right
-	
-	_player_movement()
-	move_and_slide()
 
 func _player_movement() -> void:
 	var direction_in_x := Input.get_axis(Global.M_LEFT, Global.M_RIGHT)
