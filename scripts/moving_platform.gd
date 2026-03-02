96e82a05297d1f62f3029615b468846c5a83331d
José de la Sierra <lasierradev@gmail.com>
2026-03-02 13:55:29 +0100

    [REFACT] Improved moving platforms

--- scripts/moving_platform.gd
+++ scripts/moving_platform.gd
@@ -1,8 +1,17 @@
 extends AnimatableBody2D
 
-@export var final_position_movement: Vector2
-@export var time_to_displacement: float = 2.0
+var move: bool = false
+
+@export var displacement_time: float = 2.0
+@export var wait_time: float = 1.0
+@export var target_marker: Marker2D
+
 
 func _ready() -> void:
+	var start_position = self.position
 	var tween = get_tree().create_tween()
-	tween.tween_property(self, Global.P_POSITION, final_position_movement, time_to_displacement)
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.tween_property(self, Global.P_POSITION, target_marker.global_position, displacement_time)
+	tween.tween_property(self, Global.P_POSITION, start_position, displacement_time)
+	tween.set_loops(0)
