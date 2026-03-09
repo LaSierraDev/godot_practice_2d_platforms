f897682e6657c71e8db0802b73136545a794dc8c
José de la Sierra <lasierradev@gmail.com>
2026-03-09 11:27:04 +0100

    [FEAT] Collectibles sounds implemented

--- scripts/collectibles/collectibles.gd
+++ scripts/collectibles/collectibles.gd
@@ -1,12 +1,19 @@
 extends Area2D
 
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 @export var points: int = 2
 
+
 func _ready() -> void:
 	self.area_entered.connect(_on_area_entered)
+	audio_stream_player_2d.finished.connect(_on_audio_stream_player_2d_finished)
 
 
 func _on_area_entered(_area: Area2D) -> void:
 	GameManager.update_score(points)
+	AudioManager.play_sfx(audio_stream_player_2d, AudioManager.PICKUP)
+	self.visible = false
+
+func _on_audio_stream_player_2d_finished() -> void:
 	self.queue_free()
