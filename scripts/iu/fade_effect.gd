e27c1793f89c014c10c9987198c53a58cfa20ef0
José de la Sierra <lasierradev@gmail.com>
2026-03-11 12:15:34 +0100

    [FIX] Fade-out effect time fixed

--- scripts/iu/fade_effect.gd
+++ scripts/iu/fade_effect.gd
@@ -4,5 +4,11 @@ extends Control
 
 
 func _ready() -> void:
+	SignalManager.level_completed.connect(_on_level_completed)
+	
 	self.visible = true
 	animation_player.play(Global.ANI_FADE_IN)
+
+
+func _on_level_completed() -> void:
+	animation_player.play(Global.ANI_FADE_OUT)
--- scripts/level_end.gd
+++ scripts/level_end.gd
@@ -1,10 +1,14 @@
 extends Area2D
 
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	self.area_entered.connect(_on_area_entered)
 
+
 func _on_area_entered(_area: Area2D) -> void:
+	SignalManager.level_completed.emit()
+	AudioManager.play_sfx(audio_stream_player_2d, AudioManager.SUCCESS)
 	await get_tree().create_timer(Global.TIME_FADE_IN_OUT).timeout
 	GameManager.load_next_level()
